5e843ea0db130c45ec44a125b59729f88f064d91
Sharan669 <sharansurendra1995@gmail.com>
2021-01-13 22:48:08 -0500

    Make ordersummery not to rerender for every click

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,38 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Auxillary from "../../../hoc/Auxillary";
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                <span
-                    style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>
-            );
-        })
+class OrderSummary extends Component {
 
-    return (
-        <Auxillary>
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                <span
+                    style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>
+                );
+            })
+
+        return (<Auxillary>
             <h3>Your order</h3>
             <p>A delicious burger with following ingridients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price:{props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price:{this.props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout </p>
             <Button
                 btnType="Danger"
-                clicked={props.purchaseCancelled}
+                clicked={this.props.purchaseCancelled}
             >CANCEL</Button>
             <Button
                 btnType="Success"
-                clicked={props.purchaseContinued}
+                clicked={this.props.purchaseContinued}
             >CONTINUE</Button>
-        </Auxillary>
-    )
+        </Auxillary>);
+
+    }
 
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import classes from './modal.module.css'
 import Auxillary from "../../../hoc/Auxillary";
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => {
-    return (
-        <Auxillary>
-            <Backdrop show={props.show}
-                      clicked={props.modalClosed}/>
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if (nextProps.show !== this.props.show) {
+            return true;
+        }
+        return false
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render() {
+        return (<Auxillary>
+            <Backdrop show={this.props.show}
+                      clicked={this.props.modalClosed}/>
             <div
                 className={classes.Modal}
                 style={{
-                    transform: props.show ? 'translateY(0)' : "translateY(-100vh)",
-                    opactity: props.show ? '1' : '0'
+                    transform: this.props.show ? 'translateY(0)' : "translateY(-100vh)",
+                    opactity: this.props.show ? '1' : '0'
                 }}
             >
-                {props.children}
+                {this.props.children}
             </div>
-        </Auxillary>
-    )
+        </Auxillary>)
+    }
 }
 
-export default modal;
+export default Modal;
